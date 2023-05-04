038865c907b9a919bb76586265b29e8a36a30452
Jen <73888356+berryjen@users.noreply.github.com>
2023-05-04 16:35:56 +0200

    worked on search func

--- binary-search-tree/node-js/inventory-management.js
+++ binary-search-tree/node-js/inventory-management.js
@@ -18,15 +18,18 @@ class BinarySearchTree {
         this.root = null;
     }
 
-    search(id) {
-        this.root
+    search(id, currentNode) {
+        if (id === null) {
+            return null;
+        } else {
+            return this.searchRecursive(id, currentNode)
+        }
     }
         // console.error("you must implement search");
         // 1. Start from the root node
         // 2. Compare the current node with the search value. Return early if found
         // 3. Determine if we should go left or right
         // 4. Update the current node based on the decision from (3). Rinse and repeat from (2)
-    
     searchRecursive(id, currentNode) {
         if(currentNode.val.id === id ) {
             return currentNode
